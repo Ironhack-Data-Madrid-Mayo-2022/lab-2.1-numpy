d84ab2e81c080728f884d45e0b17f421c2587b9f
lucia <luciaahab@gmail.com>
2022-06-06 16:57:33 +0200

    Hasta el 16

--- your-code/main.py
+++ your-code/main.py
@@ -1,67 +1,75 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2, 3, 5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b = np.full((5, 2, 3),1)
 
 #6. Print b.
 
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
-
+print(a.shape == b.shape)
 
 #8. Are you able to add a and b? Why or why not?
-
-
+#No te permite sumar porque no tienen la misma dimensión
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.reshape(b,(2,3,5))
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d= a+c
+print(d)
+#it works bc they have equal size
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
-
+print('this is a')
+print(a)
+print('this is b')
+print(d)
 
 
 #12. Multiply a and c. Assign the result to e.
-
+e = a*c
+print(f'esto es e')
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
-
-
-
+print(a==e) #True
+#Porque hemos multiplicado a por c y c son todo 1
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
+d_max = d.max()
+d_min = d.max()
+d_mean = d.mean()
+print(f'máximo =', d_max, 'mínimo =', d_min, 'media =', d_mean)
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f = np.empty((2,3,5))
+print(f) #Tiene números aleatorios, realmente está vacío
 
 
 
